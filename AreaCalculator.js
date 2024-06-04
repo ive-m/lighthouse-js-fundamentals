712715569f1fe3eb3544c0d251dd70ff1e5a07b9
Ivette Suarez <ivesita.maria@gmail.com>
2024-06-04 10:27:51 -0400

    Update AreaCalculator.js

--- AreaCalculator.js
+++ AreaCalculator.js
@@ -19,20 +19,7 @@ function calculateCircleArea(r) {
 
 
 
-/*their
 
-function calculateRectangleArea(l,w) {
-  if (l > 0 || w > 0) 
-   return l * w;
-}
-function calculateTriangleArea(b,h) {
-  if (b > 0 || h > 0) 
-    return b * h / 2;
-}
-function calculateCircleArea(r) {
-  if (r > 0) 
-   return Math.PI * r * r;
-}*/
 
 
 console.log(calculateRectangleArea(10, 5));     // should print 50
